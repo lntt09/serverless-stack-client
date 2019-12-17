9853547fa39656708cab8f6894554256146fec1a
Tiffany Nguyen <tiffnguyen@Tiff-MacBook.local>
2019-12-17 16:40:13 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "tiff-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://a12rmf3lre.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_1g2zN95n2",
+      APP_CLIENT_ID: "1hls420dir9sqqcn6kd8i4q4pi",
+      IDENTITY_POOL_ID: "us-east-1:a40f95a9-c96f-480d-a3b6-44dbbd0155f0"
+    }
+  };
